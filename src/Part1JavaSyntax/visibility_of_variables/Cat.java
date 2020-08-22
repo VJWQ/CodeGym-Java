f96fc622d320ee8380c5a4ac3c91f1b55c98820c
VJWQ <1336232037@qq.com>
2020-08-22 20:59:22 +0800

    Initial commit

---
+++ src/Part1JavaSyntax/visibility_of_variables/Cat.java
@@ -0,0 +1,33 @@
+package Part1JavaSyntax.visibility_of_variables;
+
+
+public class Cat {
+
+    private String name = " ";
+    private static int age = 0;
+    public static int count = 0;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        Cat.age = age;
+    }
+
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        cat1.setName("Kitty");
+        cat1.setAge(3);
+        Cat.count ++;
+        System.out.println(cat1.name + " " + cat1.age);
+        System.out.println("We have " + Cat.count + " cats!");
+
+        Cat cat2 = new Cat();
+        cat2.setName("Blue");
+        cat2.setAge(1);
+        Cat.count ++;
+        System.out.println(cat2.name + " " + cat2.age);
+        System.out.println("We have " + Cat.count + " cats!");
+    }
+}
---
+++ src/README.md
@@ -0,0 +1,7 @@
+# CodeGym-Java
+
+I use this tool for renewing and deepening my knowledge about Java.   
+
+It's a very casual and loose note. I'm only writing down some critical problems for myself. The note is **not comprehensive**.  
+
+Official website for CodeGym: [click me](https://codegym.cc/)
